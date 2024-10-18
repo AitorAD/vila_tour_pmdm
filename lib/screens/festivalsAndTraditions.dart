e4da7136c2683181872abbf8024f6615d38e7990
Aitor <aduqueagullo@gmail.com>
2024-10-18 10:07:41 +0200

    Festivals And Traditions

--- lib/screens/festivalsAndTraditions.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class FestivalsAndTraditionsScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Festivales Y Tradiciones')), body: null);
-  }
-}
