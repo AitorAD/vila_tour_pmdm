4e0328ab49c2aaadbf302b011945ef6168c59f19
Aitor <aduqueagullo@gmail.com>
2024-10-18 09:30:52 +0200

    Created FestivalsAndTraditionsScreen

---
+++ lib/screens/festivalsAndTraditions.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FestivalsAndTraditionsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Festivales Y Tradiciones')), body: null);
+  }
+}
