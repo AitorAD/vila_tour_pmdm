ba7385dff12438482d06ffdb20cba0d75c5b00b7
Aitor <aduqueagullo@gmail.com>
2024-10-19 03:19:58 +0200

    Apariencia Fiestas y Tradiciones

--- lib/src/screens/festivals.dart
+++ lib/src/screens/festivals.dart
@@ -1,42 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:vila_tour_pmdm/src/widgets/widgets.dart';
+import 'package:vila_tour_pmdm/src/utils/utils.dart';
 
 class FestivalsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text(
+          title: Text(
             'Festivales y Tradiciones',
-            style: TextStyle(color: Colors.white, fontFamily: 'PontanoSans'),
+            style: Utils.textStyleVilaTour,
           ),
           flexibleSpace: DefaultDecoration(),
           foregroundColor: Colors.white,
         ),
-        body: null);
+        body: Column(
+          children: [
+            SearchBox(),
+            FestivalBox(),
+            FestivalBox(),
+          ],
+        ));
   }
 }
 
-class DefaultDecoration extends StatelessWidget {
-  double radius;
-  DefaultDecoration({super.key, this.radius = 0});
+class FestivalBox extends StatelessWidget {
+  const FestivalBox({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(radius)),
-        gradient: LinearGradient(
-          colors: [
-            Color(0xFF4FC3F6).withOpacity(0.75),
-            Color(0xFF44C1CF).withOpacity(0.75),
-            Color(0xFF25C1CE).withOpacity(0.75),
-            Color(0xFF17BFC1).withOpacity(0.75),
-            Color(0xFF01C2A9).withOpacity(0.75),
-          ],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-        ),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          image: DecorationImage(
+              image: NetworkImage(
+                  'https://estaticos-cdn.prensaiberica.es/clip/1bffb2d9-4536-4676-9878-f7bc72f75cc4_16-9-discover-aspect-ratio_default_0.jpg'),
+              fit: BoxFit.cover)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Image(
+            image: NetworkImage(
+                'https://admin.vila.softme.es/Files/Img?url=~%2FApp_Data%2FUploadedFiles%2FJunio%202024%2F20240521041824Pebrereta.jpg&sz=100&ql=50'),
+            width: 20,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 0),
+            decoration: BoxDecoration(
+                color: Color(0xFFA0D6F1).withOpacity(0.80),
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'La Pebrereta',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'PontanoSans',
+                      fontSize: 24),
+                ),
+                Text('Plaza de la Luz - Poble Nou'),
+                Text('14 Junio'),
+                Row(
+                  children: [
+                    Row(children: [
+                      Icon(Icons.star, color: Color(0xFFEFCE4A)),
+                      Icon(Icons.star, color: Color(0xFFEFCE4A)),
+                      Icon(Icons.star, color: Color(0xFFEFCE4A)),
+                      Icon(Icons.star, color: Color(0xFFEFCE4A)),
+                      Icon(Icons.star, color: Colors.grey),
+                    ]),
+                    Text('4,9'),
+                    Icon(Icons.favorite, color: Color(0xFFD7443E))
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/src/utils/utils.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Utils {
+  static TextStyle textStyleVilaTour =
+      TextStyle(color: Colors.white, fontFamily: 'PontanoSans');
+}
