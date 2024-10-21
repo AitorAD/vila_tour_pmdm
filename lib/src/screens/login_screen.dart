6a61957f84cf53ed625618798823ce1a5779ff6e
Omar Llinares <150013139+LlinaresOmar@users.noreply.github.com>
2024-10-22 00:57:23 +0200

    trabajando en widgets y en login

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,11 +1,9 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
-import 'package:vila_tour_pmdm/src/utils/utils.dart';
 import 'package:vila_tour_pmdm/src/widgets/bar_screen.dart';
+import 'package:vila_tour_pmdm/src/widgets/button.dart';
 import 'package:vila_tour_pmdm/src/widgets/header_log.dart';
+import 'package:vila_tour_pmdm/src/widgets/input_text.dart';
 import 'package:vila_tour_pmdm/src/widgets/waves.dart';
-import 'package:vila_tour_pmdm/src/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -14,84 +12,78 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            child: Stack(
-              children: <Widget>[
-                // Background
-                Positioned.fill(
-                  child: WavesWidget(),
-                ),
-                Column(
-                  children: [
-                    // Logo y nombre de la aplicación
-                    HeaderLog(),
-                    SizedBox(height: 10),
-                    // Barra de LogIn
-                    BarScreen(labelText: "Log In"),
-                    SizedBox(height: 20),
-                    Padding(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: <Widget>[
+              // Background
+              Positioned.fill(
+                child: WavesWidget(),
+              ),
+              // BODY
+              Column(
+                children: [
+                  // Logo y nombre de la aplicación
+                  HeaderLog(),
+                  SizedBox(height: 10),
+                  // Barra de LogIn
+                  BarScreen(labelText: "Log In"),
+                  SizedBox(height: 100), // Reduce el espacio aquí para ajustar
+                  // Formulario
+                  Expanded( // Usa Expanded para ocupar el espacio restante
+                    child: Padding(
                       padding: EdgeInsets.all(20.0),
                       child: Column(
                         children: <Widget>[
+                          // Input 1
                           InputText(labelText: "E-Mail:"),
                           SizedBox(height: 25),
+                          // Input 2
                           InputText(labelText: "Contraseña:"),
                           SizedBox(height: 15),
+                          // Recuperar contraseña
                           GestureDetector(
                             onTap: () {
-                              // Acción para recuperar la contraseña
+                              print("Recuperar password"); // Llevar a pantalla de recuperar contraseña
                             },
                             child: Text(
                               '¿Has olvidado tu contraseña? Haz clic aquí',
                               style: TextStyle(color: Colors.black),
                             ),
                           ),
+                          SizedBox(height: 100), // Espacio entre el texto y los botones
+                          // Fila Botones
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              // Boton 1
+                              CustomButton(
+                                text: 'Entrar', // Texto que aparecerá en el botón
+                                onPressed: () {
+                                  // Acción para iniciar sesión
+                                  print('Botón "Entrar" presionado');
+                                },
+                              ),
+                              // Boton 2
+                              CustomButton(
+                                text: 'Registrarse', // Texto que aparecerá en el botón
+                                onPressed: () {
+                                  // Acción para iniciar sesión
+                                  print('Botón "Registrar" presionado');
+                                },
+                              ),
+                            ],
+                          ),
                         ],
                       ),
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
-    );
-  }
-}
-
-
-
-class InputText extends StatelessWidget {
-  final String labelText;
-
-  const InputText({
-    super.key,
-    required this.labelText,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              labelText,
-              style: TextStyle(
-                fontFamily: "PontanoSans",
-                fontSize: 20,
-              ),
-            )
-          ],
-        ),
-        Center(
-          child: TextField(),
-        ),
-      ]
     );
   }
 }
