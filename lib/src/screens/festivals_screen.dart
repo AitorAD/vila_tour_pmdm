81d4506cac6bd2a86ac207f6565c4f9441967cbb
Aitor <aduqueagullo@gmail.com>
2024-10-22 13:53:58 +0200

    Cambios en Festivals

--- lib/src/screens/festivals_screen.dart
+++ lib/src/screens/festivals_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:vila_tour_pmdm/src/widgets/widgets.dart';
+import 'package:vila_tour_pmdm/src/widgets/default_decoration.dart';
+import 'package:vila_tour_pmdm/src/widgets/search_box.dart';
+import 'package:vila_tour_pmdm/src/widgets/waves.dart';
 import 'package:vila_tour_pmdm/src/utils/utils.dart';
 
 class FestivalsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    /*
     var festivales = [
       {},
       {},
     ];
-
+    */
     return Scaffold(
         appBar: AppBar(
           title: Text(
@@ -19,17 +22,33 @@ class FestivalsScreen extends StatelessWidget {
           flexibleSpace: DefaultDecoration(),
           foregroundColor: Colors.white,
         ),
-        body: Column(
+        body: Stack(
           children: [
-            SearchBox(),
-            //ListView.builder(Festival)
-            FestivalBox(),
-            FestivalBox(),
+            WavesWidget(),
+            Column(
+              children: [
+                SearchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      FestivalBox(
+                        imageUrl: 'https://www.elperiodic.com/archivos/imagenes/noticias/2024/06/15/20240614-221327.jpg',
+                        title: 'Sant Miquel',
+                        location: 'Plaza de la Iglesia - La Ermita',
+                        date: '26 - 29 Septiembre',
+                        rating: 4.7,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ],
         ));
   }
 }
 
+/*
 class FestivalBox extends StatelessWidget {
   const FestivalBox({
     super.key,
@@ -92,6 +111,120 @@ class FestivalBox extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+*/
+
+class FestivalBox extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String location;
+  final String date;
+  final double rating;
+
+  FestivalBox({
+    required this.imageUrl,
+    required this.title,
+    required this.location,
+    required this.date,
+    required this.rating,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Stack(
+        children: [
+          // Imagen de fondo
+          Container(
+            height: 150,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                image: NetworkImage(imageUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Información sobre el festival
+          Positioned(
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.black.withOpacity(0.4),
+              ),
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    location,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                  Text(
+                    date,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.star, color: Colors.yellow),
+                          Icon(Icons.star, color: Colors.yellow),
+                          Icon(Icons.star, color: Colors.yellow),
+                          Icon(Icons.star, color: Colors.yellow),
+                          Icon(Icons.star_border, color: Colors.yellow),
+                        ],
+                      ),
+                      Text(
+                        rating.toString(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Icono de favorito
+          Positioned(
+            top: 10,
+            right: 10,
+            child: Icon(
+              Icons.favorite_border,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/src/widgets/search_box.dart
+++ lib/src/widgets/search_box.dart
@@ -1,31 +1,5 @@
 import 'package:flutter/material.dart';
 
-class DefaultDecoration extends StatelessWidget {
-  double radius;
-  DefaultDecoration({super.key, this.radius = 0});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(radius)),
-        gradient: LinearGradient(
-          colors: [
-            Color(0xFF4FC3F6).withOpacity(0.75),
-            Color(0xFF44C1CF).withOpacity(0.75),
-            Color(0xFF25C1CE).withOpacity(0.75),
-            Color(0xFF17BFC1).withOpacity(0.75),
-            Color(0xFF01C2A9).withOpacity(0.75),
-          ],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-        ),
-      ),
-    );
-  }
-}
-
 class SearchBox extends StatelessWidget {
   const SearchBox({
     super.key,
