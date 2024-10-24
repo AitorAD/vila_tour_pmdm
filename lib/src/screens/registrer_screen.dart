6879a58fb07d0e634acfe2aeb3f5f2bfb2f2e463
Omar Llinares <llinares.omar@gmail.com>
2024-10-24 11:39:13 +0200

    login y registrer
    login y registrer

--- lib/src/screens/registrer_screen.dart
+++ lib/src/screens/registrer_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:vila_tour_pmdm/src/screens/registrer_screen.dart';
 import 'package:vila_tour_pmdm/src/widgets/bar_screen.dart';
 import 'package:vila_tour_pmdm/src/widgets/button.dart';
 import 'package:vila_tour_pmdm/src/widgets/header_log.dart';
 import 'package:vila_tour_pmdm/src/widgets/input_text.dart';
 import 'package:vila_tour_pmdm/src/widgets/waves.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class RegistrerScreen extends StatelessWidget {
+
+ const RegistrerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +28,8 @@ class LoginScreen extends StatelessWidget {
                   HeaderLog(),
                   SizedBox(height: 10),
                   // Barra de LogIn
-                  BarScreen(labelText: "Log In"),
-                  SizedBox(height: 90), // Reduce el espacio aquí para ajustar
+                  BarScreen(labelText: "Registrarse"),
+                  SizedBox(height: 70), // Reduce el espacio aquí para ajustar
                   // Formulario
                   Expanded(
                     // Usa Expanded para ocupar el espacio restante
@@ -42,53 +42,22 @@ class LoginScreen extends StatelessWidget {
                           SizedBox(height: 25),
                           // Input 2
                           InputText(labelText: "Contraseña:"),
+                          SizedBox(height: 25),
+                          // Input 2
+                          InputText(labelText: "Repetir contraseña:"),
                           SizedBox(height: 15),
-                          // Recuperar contraseña
-                          GestureDetector(
-                            onTap: () {
-                              print(
-                                  "Recuperar password"); // Llevar a pantalla de recuperar contraseña
-                            },
-                            child: Align(
-                              alignment: Alignment
-                                  .centerLeft, // Alinea el texto a la izquierda
-                              child: Text(
-                                '¿Has olvidado tu contraseña? Haz clic aquí',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  decoration: TextDecoration
-                                      .underline, // Subraya el texto
-                                ),
-                              ),
-                            ),
-                          ),
                           SizedBox(
                               height:
-                                  70), // Espacio entre el texto y los botones
+                                  30), // Espacio entre el texto y los botones
                           // Fila Botones
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: [
-                              // Boton 1
                               CustomButton(
                                 text:
-                                    'Entrar', // Texto que aparecerá en el botón
+                                    'Crear cuenta', // Texto que aparecerá en el botón
                                 onPressed: () {
                                   // Acción para iniciar sesión
-                                  print('Botón "Entrar" presionado');
-                                },
-                              ),
-                              // Boton 2
-                              CustomButton(
-                                text:
-                                    'Registrarse', // Texto que aparecerá en el botón
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => RegistrerScreen()
-                                    )
-                                  );
                                   print('Botón "Registrar" presionado');
                                 },
                               ),
@@ -108,7 +77,7 @@ class LoginScreen extends StatelessWidget {
                                 ),
                               ),
                               Text(
-                                "  Acceder con  ",
+                                "  Registrarse con  ",
                                 style: TextStyle(
                                   fontWeight: FontWeight.bold
                                 ),
