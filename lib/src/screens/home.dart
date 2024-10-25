ca6e655d3b85911057bddcf3ac284516e0f359ec
Aitor <aduqueagullo@gmail.com>
2024-10-25 23:45:54 +0200

    Implementado modo oscuro

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,30 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:vila_tour_pmdm/src/providers/menu.dart';
+import 'package:vila_tour_pmdm/src/providers/theme_provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('VILATOUR')), body: _lista());
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+          },
+          child: Icon(Icons.dark_mode),
+        ),
+        appBar: AppBar(title: const Text('VILATOUR')),
+        body: _lista());
   }
 
   Widget _lista() {
-  return FutureBuilder(
-    future: menuProvider.cargarData(),
-    initialData: const [],
-    builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-      if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-        return ListView(
-          children: _listaItems(snapshot.data!, context),
-        );
-      } else {
-        return const Center(child: CircularProgressIndicator());
-      }
-    },
-  );
-}
-
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: const [],
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+          return ListView(
+            children: _listaItems(snapshot.data!, context),
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
 
   List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
@@ -37,9 +46,11 @@ class HomePage extends StatelessWidget {
         },
       );
 
-      opciones..add(widgetTemp)..add(const Divider());
+      opciones
+        ..add(widgetTemp)
+        ..add(const Divider());
     }
-    
+
     return opciones;
   }
 }
