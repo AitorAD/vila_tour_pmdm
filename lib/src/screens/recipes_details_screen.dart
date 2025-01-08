970d8735432b1e3af175431822a511a2dfe36ae3
Javier Otero <javii.oteero@gmail.com>
2025-01-08 13:30:22 +0100

    festivals details redone

--- lib/src/screens/recipes_details_screen.dart
+++ lib/src/screens/recipes_details_screen.dart
@@ -32,96 +32,129 @@ class _RecipeDetailsState extends State<RecipeDetails>
   Widget build(BuildContext context) {
     final Recipe recipe = ModalRoute.of(context)!.settings.arguments as Recipe;
 
-
-
     return Scaffold(
       bottomNavigationBar: CustomNavigationBar(),
       appBar: CustomAppBar(title: recipe.name),
-      body: Column(
+      body: Stack(
         children: [
-          TabBar(
-            controller: _tabController,
-            indicatorColor: const Color.fromARGB(255, 54, 71, 71),
-            tabs: const [
-              Tab(text: 'Receta'),
-              Tab(text: 'Visión General'),
-              Tab(text: 'Reseñas'),
-            ],
-          ),
-          Expanded(
-            child: TabBarView(
+        WavesWidget(),
+        Column(
+          children: [
+            TabBar(
               controller: _tabController,
-              children: [
-                // Tab 1: Receta
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: SingleChildScrollView(
+              indicatorColor: const Color.fromARGB(255, 54, 71, 71),
+              tabs: const [
+                Tab(text: 'Receta'),
+                Tab(text: 'Visión General'),
+                Tab(text: 'Reseñas'),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  // Tab 1: Receta
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          IngredientsWrap(ingredients: recipe.ingredients),
+                          const SizedBox(height: 16),
+                          const Divider(
+                            color: Colors.black,
+                            thickness: 1.0,
+                            indent: 0,
+                            endIndent: 0,
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            'Preparación',
+                            style: textStyleVilaTourTitle(color: Colors.black),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            recipe.description,
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  // Tab 2: Visión General
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        IngredientsWrap(ingredients: recipe.ingredients),
-                        const SizedBox(height: 16),
-                        const Divider(
-                          color: Colors.black,
-                          thickness: 1.0,
-                          indent: 0,
-                          endIndent: 0,
-                        ),
-                        const SizedBox(height: 10),
                         Text(
-                          'Preparación',
+                          recipe.name,
                           style: textStyleVilaTourTitle(color: Colors.black),
                         ),
-                        const SizedBox(height: 8),
-                        Text(
-                          recipe.description,
-                          style: const TextStyle(fontSize: 16),
+                        SizedBox(height: 20),
+                        Hero(
+                          tag: recipe.id, // Mismo tag que en ArticleBox
+                          child: FadeInImage(
+                            placeholder: AssetImage('assets/logo.ico'),
+                            image: recipe.images.isNotEmpty
+                                ? MemoryImage(
+                                    decodeImageBase64(recipe.images.first.path))
+                                : AssetImage('assets/logo_foreground.png')
+                                    as ImageProvider,
+                            width: double.infinity,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          ),
                         ),
+                        SizedBox(height: 20),
+                        // Row for rating stars
+                        Container(
+                          width: 300,
+                          height: 50,
+                          decoration: defaultDecoration(10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                recipe.averageScore.toString(),
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontFamily: 'PontanoSans',
+                                ),
+                              ),
+                              const SizedBox(width: 4),
+                              PaintStars(
+                                  rating: recipe.averageScore,
+                                  color: Colors.yellow),
+                              const SizedBox(width: 4),
+                              Text(
+                                '(${recipe.reviews.length})',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 16),
+                              )
+                            ],
+                          ),
+                        ),
+                        const Divider(height: 20),
+
+                        IngredientsWrap(ingredients: recipe.ingredients),
                       ],
                     ),
                   ),
-                ),
 
-                // Tab 2: Visión General
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        recipe.name,
-                        style: textStyleVilaTourTitle(color: Colors.black),
-                      ),
-                      SizedBox(height: 20),
-                      Hero(
-                        tag: recipe.id, // Mismo tag que en ArticleBox
-                        child: FadeInImage(
-                          placeholder: AssetImage('assets/logo.ico'),
-                          image: recipe.images.isNotEmpty
-                              ? MemoryImage(
-                                  decodeImageBase64(recipe.images.first.path))
-                              : AssetImage('assets/logo_foreground.png')
-                                  as ImageProvider,
-                          width: double.infinity,
-                          height: 200,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      IngredientsWrap(ingredients: recipe.ingredients),
-                    ],
-                  ),
-                ),
+                  // Tab 3: Reseñas
+                  const Center(child: Text('Contenido de las reseñas aquí')),
 
-                // Tab 3: Reseñas
-                const Center(child: Text('Contenido de las reseñas aquí')),
-
-                //ReviewsInfo(ratings: ratings)
-              ],
+                  //ReviewsInfo(ratings: ratings)
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
+          ],
+        ),
+      ]),
       /*
       floatingActionButton: Consumer<RecipesProvider>(
         builder: (context, recipesProvider, child) {
