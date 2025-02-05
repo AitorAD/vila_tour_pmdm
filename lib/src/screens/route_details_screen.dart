379e7d4e2b1adfa7e3694832cf3591051a9f3c49
Omar Llinares <llinares.omar@gmail.com>
2025-02-05 08:16:11 +0100

    fuera mapa de detales

--- lib/src/screens/route_details_screen.dart
+++ lib/src/screens/route_details_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:vila_tour_pmdm/src/services/openRoutes_service.dart';
+import 'package:vila_tour_pmdm/src/models/models.dart';
 import 'package:vila_tour_pmdm/src/widgets/widgets.dart';
 import 'package:vila_tour_pmdm/src/models/models.dart' as vilaModels;
+import 'package:latlong2/latlong.dart';
 
 class RouteDetailsScreen extends StatefulWidget {
   static final routeName = 'route_details_screen';
@@ -12,64 +12,19 @@ class RouteDetailsScreen extends StatefulWidget {
   @override
   _RouteDetailsScreenState createState() => _RouteDetailsScreenState();
 }
-
 class _RouteDetailsScreenState extends State<RouteDetailsScreen>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
-  late MapController _mapController;
-  List<LatLng> routePoints = []; // Se inicializa como lista vacía
-
-  bool isLoading = false; // Estado de carga
-  final OpenRouteService _routeService =
-      OpenRouteService(); // Instancia del servicio
-
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
-    _mapController = MapController();
-    _loadRoute(); // Carga la ruta al iniciar la pantalla
   }
-
-  // Método para cargar la ruta desde la API
-  
-Future<void> _loadRoute() async {
-  setState(() {
-    isLoading = true;
-  });
-
-  try {
-    final args = ModalRoute.of(context)?.settings.arguments;
-    if (args != null && args is vilaModels.Route) {
-      final route = args as vilaModels.Route;
-
-      // Obtener la ruta desde la API
-      final response = await _routeService.getRoute(route.places);
-      final String encodedPolyline = response['geometry']; // Extrae la geometría
-
-      print('Polyline recibida: $encodedPolyline');
-
-      // Decodificar la polyline
-      List<PointLatLng> decodedPoints = PolylinePoints().decodePolyline(encodedPolyline);
-      List<LatLng> points = decodedPoints.map((point) => LatLng(point.latitude, point.longitude)).toList();
-
-      if (points.isEmpty) {
-        print('Error: La polyline decodificada está vacía.');
-      }
-
-      setState(() {
-        routePoints = points;
-      });
-    }
-  } catch (e) {
-    print('Error al cargar la ruta: $e');
-  } finally {
-    setState(() {
-      isLoading = false;
-    });
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
-}
-
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)?.settings.arguments;
@@ -90,22 +45,29 @@ Future<void> _loadRoute() async {
       bottomNavigationBar: CustomNavigationBar(),
       body: Stack(
         children: [
+          // Fondo con WavesWidget
           WavesWidget(),
+          // Contenido principal
           Column(
             children: [
+              // Barra superior con flecha de retroceso
               BarScreenArrow(labelText: route.name, arrowBack: true),
+              // Pestañas
               TabBar(
                 controller: _tabController,
                 tabs: const [
-                  Tab(text: 'Mapa'),
+                  Tab(text: 'Información'),
                   Tab(text: 'Lugares'),
                 ],
               ),
+              // Área deslizable con TabBarView
               Expanded(
                 child: TabBarView(
                   controller: _tabController,
                   children: [
+                    // Primera pantalla
                     _buildFirstScreen(route),
+                    // Segunda pantalla
                     _buildSecondScreen(route),
                   ],
                 ),
@@ -116,65 +78,14 @@ Future<void> _loadRoute() async {
       ),
     );
   }
-
 // Método para construir la primera pantalla (Mapa)
   Widget _buildFirstScreen(vilaModels.Route route) {
-    if (isLoading) {
-      return Center(child: CircularProgressIndicator());
-    }
-
-    if (routePoints.isEmpty) {
-      return Center(child: Text('No hay puntos de ruta disponibles.'));
-    }
-
-    // Calcular los límites del mapa con los puntos obtenidos de la API
-    final bounds = LatLngBounds.fromPoints(routePoints);
-
-    return FlutterMap(
-      mapController: _mapController,
-      options: MapOptions(
-        initialCenter: bounds.center,
-        initialZoom: 13.0,
-        minZoom: 10.0,
-        maxZoom: 18.0,
-        onMapReady: () {
-          _mapController.move(bounds.center, 13.0);
-        },
-      ),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          subdomains: ['a', 'b', 'c'],
-          userAgentPackageName: 'com.example.vila_tour_pmdm',
-        ),
-        // Dibujar la ruta con los puntos devueltos por la API
-        PolylineLayer(
-          polylines: [
-            Polyline(
-              points: routePoints, // Se usa la ruta obtenida de la API
-              color: Colors.blue,
-              strokeWidth: 4.0,
-            ),
-          ],
-        ),
-        // Dibujar los marcadores de los lugares
-        MarkerLayer(
-          markers: route.places.map((place) {
-            return Marker(
-              width: 80.0,
-              height: 80.0,
-              point:
-                  LatLng(place.coordinate.latitude, place.coordinate.longitude),
-              child: Icon(
-                Icons.place,
-                color: Colors.red,
-              ),
-            );
-          }).toList(),
-        ),
-      ],
+    return Container(
+      child: Text(route.name),
     );
   }
+
+
 
   // Método para construir la segunda pantalla (Lista de lugares)
   Widget _buildSecondScreen(vilaModels.Route route) {
@@ -183,8 +94,6 @@ Future<void> _loadRoute() async {
         children: [
           ListView.builder(
             shrinkWrap: true,
-            physics:
-                NeverScrollableScrollPhysics(), // Evita conflictos con el scroll padre
             itemCount: route.places.length,
             itemBuilder: (context, index) {
               return ArticleBox(article: route.places[index]);
